53077a7346f67d6901f26fc5ee0c7d1e7b826396
Dinu-Sri <dinu@wyb.ac.lk>
2021-09-24 17:34:17 +0530

    post styles updated. But grid view problem remain

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php get_header(); ?>
 
  
-
-<div class="w3-row-padding">
+<div class="w3-main w3-content w3-padding" style="max-width:70%;margin-top:100px"> 
  <?php if(have_posts()) : ?> 
 
            <?php while(have_posts()): the_post(); ?> 
-           	<div class="w3-third w3-container" >
-           		<div class="w3-hover-shadow w3-center w3-round-xlarge">
+           	<div class=" w3-third w3-container ">
+           		<div class="pushup_anim w3-center " >
            	
-         			 <?php the_post_thumbnail('full', array('class' => 'w3-round-xlarge w3-image w3-card')); ?>
-
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <?php ?> 
-             <a class="w3-btn w3-blue w3-round-xxlarge" href="<?php the_permalink(); ?>"> Read More</a>
+         			<?php the_post_thumbnail('full', array('class' => 'w3-round-xlarge w3-image w3-card')); ?>
+            		<h3 class = "post_titles" ><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3><?php ?>
+					
             </div>
              </div>
          <?php endwhile; ?>
@@ -25,6 +22,13 @@
          <?php endif; ?>
 </div>
 
-  
+
+
+
+
+
+
+
+
 
 <?php get_footer(); ?>
